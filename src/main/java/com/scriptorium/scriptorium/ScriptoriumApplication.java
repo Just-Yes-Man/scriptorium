b3d82b5a64293b81e106843805dba11f7d07c297
Andryk Giovany <andrykgiovanytc@gmail.com>
2025-05-04 23:46:44 -0600

    paso a 4200

--- src/main/java/com/scriptorium/scriptorium/ScriptoriumApplication.java
+++ src/main/java/com/scriptorium/scriptorium/ScriptoriumApplication.java
@@ -18,7 +18,7 @@ public class ScriptoriumApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8081")
+						.allowedOrigins("http://localhost:4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowCredentials(true);
 			}
